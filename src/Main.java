b0cbe85af9cf957b69cf3feecc1f3e9f4fae346b
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-12 21:51:34 +0300

    feat(Main): added sorting and contains methods
    - added entry point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Airplane[] airplanes = {
+                new Airplane("Ukraine", "Antonov", "An-225 Mriya", 850, 15400),
+                new Airplane("USA", "Boeing", "747", 988, 18300),
+                new Airplane("France", "Airbus", "A380", 1020, 15200),
+                new Airplane("USA", "Lockheed", "SR-71 Blackbird", 3529, 5632),
+                new Airplane("USA", "Boeing", "B-52 Stratofortress", 988, 16000),
+        };
+
+        System.out.println("Airplanes before sorting:");
+        printAirplanes(airplanes);
+
+        Arrays.sort(airplanes);
+
+        System.out.println("\nAirplanes after sorting:");
+        printAirplanes(airplanes);
+
+        Airplane target = new Airplane("Ukraine", "Antonov", "An-225 Mriya", 850, 15400);
+        Airplane airplane = contains(airplanes, target);
+
+        if (airplane != null) System.out.println("\nAirplane found: " + airplane);
+        else System.out.println("\nAirplane not found: " + target);
+
+    }
+
+    private static void printAirplanes(Airplane[] airplanes) {
+        for (Airplane airplane : airplanes) {
+            System.out.println(airplane.toString());
+        }
+    }
+
+    private static Airplane contains(Airplane[] airplanes, Airplane target) {
+        for (Airplane airplane : airplanes) {
+            if (airplane.equals(target)) {
+                return target;
+            }
+        }
+        return null;
     }
 }
